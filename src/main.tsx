519a5be6706409462e446f8d4e2e6cfb44c9ea31
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-11-10 16:34:38 +0200

    refactor: extract root element creation for better code organization

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import App from "./App.tsx";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <HashRouter>
     <App />
   </HashRouter>
